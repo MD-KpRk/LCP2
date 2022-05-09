30a4dff8333514d387ffc1e78a931e9247688db4
KpRk <griferad@gmail.com>
2022-05-09 17:07:23 +0300

    Добавил проект библиотеки классов. Добавил класс для определения локального ip адреса машины

---
+++ Library/MyIP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class MyIP
+    {
+        public IPAddress? IPv6;
+        public IPAddress? IPv4;
+
+        public MyIP()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (var ip in host.AddressList)
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    IPv4 = ip;
+                    break;
+                }
+
+
+            foreach (var ip in host.AddressList)
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+                {
+                    IPv6 = ip;
+                    break;
+                }
+        }
+    }
+}
